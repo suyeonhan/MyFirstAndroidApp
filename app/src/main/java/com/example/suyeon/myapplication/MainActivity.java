960c3b35bf0170a4d942f75ea20fecaa1edb8823
suyeon.han <gkstndus6156@gmail.com>
2017-01-23 10:36:06 +0900

    firest commit

---
+++ app/src/main/java/com/example/suyeon/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.suyeon.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btn);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "버튼을 클릭하셨습니다.", Toast.LENGTH_SHORT).show();
+    }
+}
