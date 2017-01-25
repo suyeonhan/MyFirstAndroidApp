a70c7a11e9fba19c24697aab80ea6da8de798215
suyeon.han <gkstndus6156@gmail.com>
2017-01-25 17:06:07 +0900

    스코어 보드 예제 연습

---
+++ app/src/main/java/com/example/suyeon/myapplication/court_counter_activity.java
@@ -0,0 +1,13 @@
+package com.example.suyeon.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class court_counter_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_court_counter_activity);
+    }
+}
